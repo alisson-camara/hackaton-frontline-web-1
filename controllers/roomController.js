f57c7f19dc25b6a73f98df20504b00cdeae5df01
Thiago Barros <thiago.barros@ab-inbev.com>
2024-12-18 11:10:59 -0300

    Changing roomController.js

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,11 +1,9 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-  res.render('pages/index')
-})
+app.post("/create-room", (req, res) => {
+  res.send({
+    message: "New user was added to the list",
+  });
+});
